af160d8623dcd5b303b5f610357d510913eb604a
Phelipe Rocha <phelipeafonso@gmail.com>
2019-06-09 13:28:55 -0300

    Removing unnecessary ;

--- itauscraper.js
+++ itauscraper.js
@@ -12,45 +12,45 @@ const stepLogin = async (page, options) => {
     await page.goto(options.itau.url)
     console.log('Homepage loaded.')
     await page.type('#agencia', options.branch)
-    await page.type('#conta', options.account);
-    console.log('Account and branch number has been filled.');
-    await page.waitFor(500);
-    await page.click('#btnLoginSubmit');
-    console.log('Opening password page...');
+    await page.type('#conta', options.account)
+    console.log('Account and branch number has been filled.')
+    await page.waitFor(500)
+    await page.click('#btnLoginSubmit')
+    console.log('Opening password page...')
 
     // Input password
-    await page.waitFor('div.modulo-login');
-    console.log('Password page loaded.');
-    let passwordKeys = await mapPasswordKeys(page);
-    let keyClickOption = { delay: 300 };
-    await page.waitFor(500);
-    console.log('Filling account password...');
+    await page.waitFor('div.modulo-login')
+    console.log('Password page loaded.')
+    let passwordKeys = await mapPasswordKeys(page)
+    let keyClickOption = { delay: 300 }
+    await page.waitFor(500)
+    console.log('Filling account password...')
     for (const digit of options.password.toString()) {
-        await passwordKeys[digit].click(keyClickOption);
+        await passwordKeys[digit].click(keyClickOption)
     }
-    console.log('Password has been filled...login...');
-    await page.waitFor(500);
-    page.click('#acessar', keyClickOption);
-    await page.waitFor('#sectionHomePessoaFisica');
-    console.log('Logged!');
-};
+    console.log('Password has been filled...login...')
+    await page.waitFor(500)
+    page.click('#acessar', keyClickOption)
+    await page.waitFor('#sectionHomePessoaFisica')
+    console.log('Logged!')
+}
 
 const stepExportStatement = async (page, options) => {
     await page.waitFor(5000)
-    await page.waitFor('div.botoes.clear.clearfix.no-margem-baixo');
+    await page.waitFor('div.botoes.clear.clearfix.no-margem-baixo')
 
     // Get balance
-    console.log('Getting your balance...');
-    const balanceElement = await page.$("#saldo");
+    console.log('Getting your balance...')
+    const balanceElement = await page.$("#saldo")
     const balanceValue = await page.evaluate(balanceElement => balanceElement.textContent, balanceElement)
     console.log(`Your balance is ${balanceValue.toString().replace(/\s+/g, ' ').trim()}`)
     balanceElement.dispose()
 
     // Go to statement page
-    console.log('Opening statement page...');
-    await page.click('div.botoes.clear.clearfix.no-margem-baixo');
+    console.log('Opening statement page...')
+    await page.click('div.botoes.clear.clearfix.no-margem-baixo')
     await page.waitFor(5000)
-    console.log('Statement page loaded.');
+    console.log('Statement page loaded.')
 
     console.log(`Selection last ${options.days} days`)
     await page.select('#select-5', options.days.toString())
@@ -60,35 +60,35 @@ const stepExportStatement = async (page, options) => {
     await page._client.send('Page.setDownloadBehavior', {
         behavior: 'allow',
         downloadPath: TEMP_FOLDER,
-    });
+    })
 
     console.log('Running downloading function')
     await page.evaluate(() => exportarExtratoArquivo('formExportarExtrato', 'txt'))
     await page.waitFor(8000)
     console.log('Finish download')
-};
+}
 
 const stepClosePossiblePopup = async (page) => {
     await page.waitForSelector('div.mfp-wrap', { timeout: 4000 })
         .then(() => page.evaluate(() => popFechar()))
-        .catch(() => { });
-};
+        .catch(() => { })
+}
 
 const mapPasswordKeys = async (page) => {
-    let keys = await page.$$('.teclas .tecla');
-    let keyMapped = {};
+    let keys = await page.$$('.teclas .tecla')
+    let keyMapped = {}
 
     for (const key of keys) {
-        let text = await page.evaluate(element => element.textContent, key);
+        let text = await page.evaluate(element => element.textContent, key)
         if (text.includes('ou')) {
-            let digits = text.split('ou').map(digit => digit.trim());
-            keyMapped[digits[0]] = key;
-            keyMapped[digits[1]] = key;
+            let digits = text.split('ou').map(digit => digit.trim())
+            keyMapped[digits[0]] = key
+            keyMapped[digits[1]] = key
         }
     }
 
-    return keyMapped;
-};
+    return keyMapped
+}
 
 const readFile = () => {
     const files = fs.readdirSync(TEMP_FOLDER)
@@ -130,27 +130,27 @@ const sendToFinancialDataProcessor = (data) => {
 }
 
 const scraper = async (options) => {
-    console.log('Starting Itaú scraper...');
-    console.log('Account Branch Number:', options.branch);
-    console.log('Account number:', options.branch);
-    console.log('Transaction log days:', options.days);
+    console.log('Starting Itaú scraper...')
+    console.log('Account Branch Number:', options.branch)
+    console.log('Account number:', options.branch)
+    console.log('Transaction log days:', options.days)
 
-    console.debug('Puppeter - options', options.puppeteer);
-    const browser = await puppeteer.launch(options.puppeteer);
+    console.debug('Puppeter - options', options.puppeteer)
+    const browser = await puppeteer.launch(options.puppeteer)
 
-    const page = await browser.newPage();
-    console.debug('Viewport - options', options.viewport);
-    page.setViewport(options.viewport);
+    const page = await browser.newPage()
+    console.debug('Viewport - options', options.viewport)
+    page.setViewport(options.viewport)
 
-    await stepLogin(page, options);
-    await stepClosePossiblePopup(page);
-    await stepExportStatement(page, options);
+    await stepLogin(page, options)
+    await stepClosePossiblePopup(page)
+    await stepExportStatement(page, options)
     const data = readFile()
     await sendToFinancialDataProcessor(data)
 
-    await browser.close();
+    await browser.close()
 
-    console.log('Itaú scraper finished.');
-};
+    console.log('Itaú scraper finished.')
+}
 
-module.exports = scraper;
+module.exports = scraper
